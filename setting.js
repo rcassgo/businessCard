346ecbdee56df646d0144835366570d5e5370fde
rcassgo <127597198+rcassgo@users.noreply.github.com>
2024-10-02 16:13:43 +0900

    update setting.js

--- setting.js
+++ setting.js
@@ -5,7 +5,7 @@ export const job = "무직";
 // 핸드폰 번호
 export const phoneNumber = "010-7652-0000";
 // 이메일
-export const emailAddress = "orcaasgo@gmail.com";
+export const emailAddress = "testtest@gmail.com";
 // 사이트 링크
 export const websiteURL = "http://gogoring.shop";
 
