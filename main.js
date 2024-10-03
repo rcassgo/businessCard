3547509f453e5dd553956b7b49d9bece76ad9f2c
rcassgo <127597198+rcassgo@users.noreply.github.com>
2024-10-03 16:39:10 +0900

    change background2

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ document.querySelector(".fullName").textContent = set.name;
 document.querySelector(".position").textContent = set.position;
 document.querySelector(".jobTitle").textContent = set.job;
 
-document.querySelector(".phone-icon").href = `tel:${set.phoneNumber}`;
+document.querySelector(".phone-link").href = `tel:${set.phoneNumber}`;
 document.querySelector(".phone-link").textContent = set.phoneNumber;
 
-document.querySelector(".email-icon").href = `mailto:${set.emailAddress}`;
+document.querySelector(".email-link").href = `mailto:${set.emailAddress}`;
 document.querySelector(".email-link").textContent = set.emailAddress;
 
-document.querySelector(".website-icon").href = set.websiteURL;
+document.querySelector(".website-link").href = set.websiteURL;
 document.querySelector(".website-link").textContent = set.websiteURL;
 
 document.querySelector(".facebook-icon").src = set.facebookImg;
